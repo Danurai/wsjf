d3ed685301dba70c993c6292a14e9200ee3c67e3
Danurai <Danurai@users.noreply.github.com>
2024-05-22 13:11:48 +0100

    Update client.js
    Fix Brackets

--- js/client.js
+++ js/client.js
@@ -7,59 +7,59 @@ var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd
 var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
 
 TrelloPowerUp.initialize({
-	'card-buttons': function(t, options) {
-		return [{
-			icon: BLACK_ROCKET_ICON,
-			text: 'WSJF',
-      callback: function(t) {
+  'card-buttons': function (t, options) {
+    return [{
+      icon: BLACK_ROCKET_ICON,
+      text: 'WSJF',
+      callback: function (t) {
         return t.popup({
           title: "WSJF",
           url: 'estimate.html',
         });
       }
-		}];
-	},
-  'card-badges': function(t, options) {
-    return t.get('card', 'shared',  'wsjf')
-    .then(function(wsjf) {
-      return [{
-        icon:  WHITE_ROCKET_ICON,
-         text: wsjf  || 'No WSJF!',
-        color: wsjf ? 'green' : 'red',
     }];
-  });
-          },
-   'card-detail-badges': function(t, options) {
-    return t.get('card', 'shared',  'wsjf')
-    .then(function( wsjf) {
-      return [{
-        title: 'WSJF',
-        text: wsjf  || 'No WSJF!',
-        color: wsjf ? 'green' : 'red',
-        callback: function(t) {
-          return t.popup({
-            title: "WSJF",
-            url: 'estimate.html',
-          });
-        }
-      }]
-    });
-'list-sorters': function (t) {
-    return t.list('name', 'id')
-    .then(function (list) {
-      return [{
-        text: "WSJF",
-        callback: function (t, opts) {
-          // Trello will call this if the user clicks on this sort
-          // opts.cards contains all card objects in the list
-          var sortedCards = opts.cards.sort((a, b) => a.wsjf - b.wsjf);
-          return {
-            sortedIds: sortedCards.map((c) => return c.id;);
-          };
-        }
-      }];
   },
-  'show-settings': function(t, options){
+  'card-badges': function (t, options) {
+    return t.get('card', 'shared', 'wsjf')
+      .then(function (wsjf) {
+        return [{
+          icon: WHITE_ROCKET_ICON,
+          text: wsjf || 'No WSJF!',
+          color: wsjf ? 'green' : 'red',
+        }];
+      });
+  },
+  'card-detail-badges': function (t, options) {
+    return t.get('card', 'shared', 'wsjf')
+      .then(function (wsjf) {
+        return [{
+          title: 'WSJF',
+          text: wsjf || 'No WSJF!',
+          color: wsjf ? 'green' : 'red',
+          callback: function (t) {
+            return t.popup({
+              title: "WSJF",
+              url: 'estimate.html',
+            });
+          }
+        }]
+      });
+  },
+  'list-sorters': function (t) {
+    return t.list('name', 'id')
+      .then(function (list) {
+        return [{
+          text: "WSJF",
+          callback: function (t, opts) {
+            // Trello will call this if the user clicks on this sort
+            // opts.cards contains all card objects in the list
+            var sortedCards = opts.cards.sort((a, b) => a.wsjf - b.wsjf);
+            return {
+              sortedIds: sortedCards.map((c) => return c.id;);
+            };
+          });
+  },
+  'show-settings': function (t, options) {
     // when a user clicks the gear icon by your Power-Up in the Power-Ups menu
     // what should Trello show. We highly recommend the popup in this case as
     // it is the least disruptive, and fits in well with the rest of Trello's UX
@@ -69,5 +69,5 @@ TrelloPowerUp.initialize({
       height: 184 // we can always resize later
     });
   }
-          
+
 });
